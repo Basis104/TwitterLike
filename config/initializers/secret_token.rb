a642187a0d83f53c575ff5105d7e10be3c5d7e0e
Yu SUGIMOTO <k138563@ie.u-ryukyu.ac.jp>
2014-03-17 03:27:50 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterLike::Application.config.secret_key_base = 'dfa981d7ce649859e199eaf5ec3cf0960f1813b2180daaf910b9bee44fc91d22f82c16f8f209e4fda36da6ae53cf9487a41655e00bffbeb4d7f24972678f0863'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterLike::Application.config.session_store :cookie_store, key: '_TwitterLike_session'
